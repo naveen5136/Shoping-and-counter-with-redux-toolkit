402ce733412c732a0cdfc7593807a784224dbb9e
Naveen <naveen.yadav10@wipro.com>
2022-12-03 21:55:32 +0530

    APi called with createASYNCThunk API

--- src/ProductThunks/ProductThunk.js
+++ src/ProductThunks/ProductThunk.js
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 export const CONSTANT = {
      IDLE:'idle',
@@ -15,31 +15,53 @@ const productThunks = createSlice({
     name:"getProducts",
     initialState,
     reducers:{
-        setProducts(state,action){
+        // setProducts(state,action){
+        //     state.data = action.payload
+        // },
+        // setStatus(state,action){
+        //    state.status = action.payload
+        // }
+    },
+    extraReducers: (builder)=>{
+        builder
+        .addCase((fetchProducts.pending),(state,action)=>{
+            state.status = CONSTANT.LOADING
+        })
+        .addCase((fetchProducts.fulfilled),(state,action)=>{
             state.data = action.payload
-        },
-        setStatus(state,action){
-           state.status = action.payload
-        }
+            state.status = CONSTANT.IDLE
+        })
+        .addCase((fetchProducts.rejected),(state,action)=>{
+            state.status = CONSTANT.ERROR
+        })
     }
 })
+
 export const {setProducts,setStatus} = productThunks.actions
 export default productThunks.reducer
 
-export function fetchProducts(){
-      return async function fetchProductsThunk(dispatch,getState){
-        dispatch(setStatus(CONSTANT.LOADING))
-           try 
-           {
-                  const getRes= await fetch('https://fakestoreapi.com/products')
-                  const getproData = await getRes.json()
-                  console.log("thunk",getproData)
-                  dispatch(setProducts(getproData))
-                  dispatch(setStatus(CONSTANT.IDLE))
-           }
-           catch(err) {
-                 dispatch(setStatus(CONSTANT.ERROR))
-           }
-      }
-}
+export const fetchProducts= createAsyncThunk('products/fetch', async ()=>{
+    const getRes= await fetch('https://fakestoreapi.com/products')
+    const getproData = await getRes.json()
+    return getproData
+})
+
+// WITH SIMPLE FUNCTION 
+
+// export function fetchProducts(){
+//       return async function fetchProductsThunk(dispatch,getState){
+//         dispatch(setStatus(CONSTANT.LOADING))
+//            try 
+//            {
+//                   const getRes= await fetch('https://fakestoreapi.com/products')
+//                   const getproData = await getRes.json()
+//                   console.log("thunk",getproData)
+//                   dispatch(setProducts(getproData))
+//                   dispatch(setStatus(CONSTANT.IDLE))
+//            }
+//            catch(err) {
+//                  dispatch(setStatus(CONSTANT.ERROR))
+//            }
+//       }
+// }
 
